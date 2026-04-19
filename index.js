e846f0cf9c949a4c5f929fbbe663b8a5b2f787ee
Miguel Aviles <miguelaviles@Mac.lan>
2026-04-19 18:34:30 -0500

    YourMessage

---
+++ index.js
@@ -0,0 +1,25 @@
+// 
+// 
+// 
+
+function contact(event) {
+    event.preventDefault();
+const loading = document.querySelector('.modal__overlay--loading');
+    constsuccess = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+    emailjs
+     .sendForm(
+    'Maviles966', 
+     'template_75o7qsc',
+     event.target,
+     'KvLscmd-2CDUOB2ZF',
+     ).then(() => {
+      loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+     }).catch(() => {
+       loading.classList.remove("modal__overlay--visible");
+       alert(
+        "The email service is temporarily unavailable. Please contact me directly on avilesmiguel1996@yahoo.com"
+       );
+     })
+}
